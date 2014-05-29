3e9fbc03468ad8a9e72e471e658eb77399f75860
Jason Vasquez <fjvasquez@aol.com>
2014-05-28 21:02:27 -0700

    Ruby practices

---
+++ ex25.rb
@@ -0,0 +1,40 @@
+#Even more practice with Ruby interpreter
+
+module Ex25
+	def self.break_words(stuff)
+		#This function will break up words.
+		words = stuff.split('')
+		words
+	end
+
+	def self.sort_words(words)
+		# Sorts words
+		words.sort()
+	end
+
+	def self.print_first_word(words)
+		# Prints the first word and the shifts the others down by one.
+		word = words.shift()
+		puts word
+	end
+
+	def self.print_last_word(words)
+		# Prints the last word after popping it off at the end.
+		word = words.pop()
+		puts word
+	end
+
+	def self.sort_sentence(sentence)
+		# Takes in a full sentence and returns the sorted words.
+		words = break_words(sentence)
+		sort_words(words)
+	end
+
+	def self.print_first_and_last(sentence)
+		# Prints the first and last words of the sentence.
+		words = sort_sentence(sentence)
+		print_first_word(words)
+		print_last_word(words)
+	end
+end
+
